2c4cc7f1fd239aaa091388bff6ac5dd0caae75fa
Programmer434 <jrbatts3@gmail.com>
2022-11-24 18:33:03 +0000

    get it working in lambda

--- src/autotrader.ts
+++ src/autotrader.ts
@@ -6,58 +6,64 @@ const logger = bunyan.createLogger({ name: 'scrapertron' })
 async function start() {
     logger.info('Starting logger scraper')
 
-    let browser: puppeteer.Browser
-    if (process.env.IS_CI === 'true') {
-        logger.info('running in CI env ')
+    try {
+        let browser: puppeteer.Browser
+        if (process.env.IS_CI === 'true') {
+            logger.info('running in CI env ')
 
-        browser = await puppeteer.launch({
-            executablePath: '/usr/bin/google-chrome-stable',
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--single-process',
-            ],
-        })
-        logger.info('browser started')
-    } else {
-        logger.info('running locally')
-        browser = await puppeteer.launch()
+            browser = await puppeteer.launch({
+                executablePath: '/usr/bin/google-chrome-stable',
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--single-process',
+                ],
+            })
+            logger.info('browser started')
+        } else {
+            logger.info('running locally')
+            browser = await puppeteer.launch()
+        }
+
+        const page = await browser.newPage()
+        logger.info('page created')
+        //This is needed otherwise request gets blocked
+        page.setUserAgent(
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+        )
+
+        await page.goto(
+            process.env.TARGET_URL ||
+                'https://www.autotrader.co.uk/car-search?postcode=RG5%203AY&make=Audi&model=A4&price-from=5000&price-to=10000&include-delivery-option=on&advertising-location=at_cars&page=1'
+        )
+
+        logger.info('loaded page')
+        const carPrices = await getPrices(page)
+
+        const subStories = await getCarSubDetails(page)
+
+        let listOfCarDetails = []
+        for (let index = 0; index < subStories.length; index++) {
+            const element = subStories[index]
+            let finalObj = {}
+            element.map((element) => {
+                let carAttribute = carDescriptionDiscovery(element)
+                if (carAttribute) {
+                    finalObj = { ...finalObj, [carAttribute]: element }
+                }
+            })
+            //add in the price
+            finalObj = { ...finalObj, price: carPrices[index] }
+            listOfCarDetails.push(finalObj)
+        }
+
+        logger.info('Successfully scraped page')
+        logger.info(listOfCarDetails[0])
+        await browser.close()
+    } catch (error) {
+        logger.error('failed to get to required page')
+        throw error
     }
-
-    const page = await browser.newPage()
-    logger.info('page created')
-    //This is needed otherwise request gets blocked
-    page.setUserAgent(
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
-    )
-
-    await page.goto(
-        process.env.TARGET_URL ||
-            'https://www.autotrader.co.uk/car-search?postcode=RG5%203AY&make=Audi&model=A4&price-from=5000&price-to=10000&include-delivery-option=on&advertising-location=at_cars&page=1'
-    )
-    logger.info('loaded page')
-    const carPrices = await getPrices(page)
-
-    const subStories = await getCarSubDetails(page)
-
-    let listOfCarDetails = []
-    for (let index = 0; index < subStories.length; index++) {
-        const element = subStories[index]
-        let finalObj = {}
-        element.map((element) => {
-            let carAttribute = carDescriptionDiscovery(element)
-            if (carAttribute) {
-                finalObj = { ...finalObj, [carAttribute]: element }
-            }
-        })
-        //add in the price
-        finalObj = { ...finalObj, price: carPrices[index] }
-        listOfCarDetails.push(finalObj)
-    }
-
-    logger.info('Successfully scraped page')
-    logger.info(listOfCarDetails[0])
-    await browser.close()
 }
 
 export async function getCarSubDetails(
@@ -125,3 +131,6 @@ export function carDescriptionDiscovery(
 }
 
 start()
+    .then(() => console.log('pupperter done'))
+    .catch((e) => console.log('it errored'))
+    .finally(() => console.log('finished'))
